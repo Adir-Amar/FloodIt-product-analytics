6b32bc178f3e763374a04daee5462eae4327557b
Adir Amar <adiramar777@gmail.com>
2026-08-02 00:58:58 +0300

    Add mode param census

---
+++ sql/mode_param_census.sql
@@ -0,0 +1,20 @@
+SELECT event_name, param.key AS parameter_name, COUNT(*) AS n_events_with_it,  COUNT(DISTINCT COALESCE(
+    param.value.string_value, 
+    CAST(param.value.int_value AS STRING), 
+    CAST(param.value.float_value AS STRING), 
+    CAST(param.value.double_value AS STRING)
+  )) AS n_distinct_values,
+     ANY_VALUE(COALESCE(
+    param.value.string_value, 
+    CAST(param.value.int_value AS STRING), 
+    CAST(param.value.float_value AS STRING), 
+    CAST(param.value.double_value AS STRING)
+  )) AS example_value
+
+FROM `firebase-public-project.analytics_153293282.events_*`CROSS JOIN UNNEST(event_params) AS param
+
+WHERE event_name IN ('level_start', 'level_start_quickplay')
+
+GROUP BY event_name, parameter_name
+
+ORDER BY event_name;
